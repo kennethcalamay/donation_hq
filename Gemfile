8f4ceab2dbab69f9d40e30f4d76b95747c34cb59
Kenneth Calamay <dev+kenneth@proudcloud.net>
2012-11-14 14:45:44 +0800

    Initial import.

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3', :group => :development
+gem 'pg', :group => :production
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem "cucumber-rails"
+  gem "capybara"
+  gem "rspec-rails", ">= 2.0.1"
+  gem "database_cleaner"
+end
+
+gem 'jquery-rails'
+gem "slim"
+gem "slim-rails"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DonationHq::Application.config.secret_token = 'a7e833af812c524640aacf029dfca5c48874f314c95eebf3cc9943ef894405a44013f41b7bdbef6b3d3e68642dcce2f63bd62455b89d3a751baa13a8f826916b'
